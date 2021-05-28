46d7de15c1272c223c3b98b58f080afbd7c49cc8
Denis <qwertyuiop852002058@yandex.ru>
2021-05-28 17:08:58 +0300

    2

--- _template/scripts/main.js
+++ _template/scripts/main.js
@@ -1,3 +1,19 @@
+DEFAILT_SETTINGS = {
+    "CarNumber": {
+        timeShow: 7,
+        countRound: 5,
+    },
+    "MultiGame": {
+        countRound: 10,
+        games: ["CarNumber"],
+    },
+    games: [{
+        name: "CarNumber",
+        title: "Автономильный номер",
+        description: "Запомните, а потом вспомните, автоноьильный номер в формате России",
+    }],
+};
+
 function getRandomInt(max, min) {
     return Math.floor(Math.random() * (max - min - 1)) + min + 1;
 };
@@ -7,49 +23,73 @@ function getRandomVal(values) {
     return array[getRandomInt(0, array.length - 1)];
 };
 
+function replaceSimilarLetters(str) {
+    const E = "ABEKMHOPCTYX";
+    const R = "АВЕКМНОРСТУХ";
+    return str.replaceAll(new RegExp(`[${R}]`, "g"), o => E[R.indexOf(o)]);
+};
+
 const vueApp = Vue.createApp({
     data: () => ({
-        screen: "",
-        type: "none",
+        settings: DEFAILT_SETTINGS,
+        screen: "none",
         options: {},
         time: 5,
         score: 0,
         count: 0,
     }),
-    complete: {
-        hash: {
-            get: () => location.hash.slice(1),
-            set: (newValue) => location.hash = newValue,
-        },
-    },
     methods: {
         async startCarNumberShowScreen() {
-            const L = "АВЕКМНОРСТУХ";
-            const number = (getRandomVal(L) + getRandomInt(100, 999) +
-                getRandomVal(L) + getRandomVal(L) + " " + getRandomInt(1, 999));
+            const L = "ABEKMHOPCTYX";
+            let number = getRandomVal(L) + getRandomInt(100, 999);
+            number += getRandomVal(L) + getRandomVal(L) + " " + getRandomInt(1, 999);
             this.options.number = number;
-            this.time = 5;
-            this.screen = "car-number-show";
+            this.time = DEFAILT_SETTINGS.CarNumber.timeShow;
+            this.screen = "carNumberShow";
             while (this.time) {
                 await new Promise(resolve => setTimeout(resolve, 1000));
                 this.time -= 1;
             };
             this.screen = "none";
             this.options = {};
+            number = number.replaceAll(/\s*/g, "").toUpperCase();
+            number = replaceSimilarLetters(number);
             return number;
         },
         async startCarNumberGuessScreen() {
-            this.screen = "car-number-guess";
+            this.screen = "carNumberGuess";
+            setTimeout(() => this.$refs.input.focus(), 500);
             await new Promise(resolve => this.$refs.btn.onclick = resolve);
-            const answer = this.options.answer.toUpperCase();
+            let answer = this.options.answer || "";
+            answer = answer.replaceAll(/\s*/g, "").toUpperCase();
+            answer = replaceSimilarLetters(answer);
             this.screen = "none";
             this.options = {};
             return answer;
         },
+        async startPlainGame(nameGame) {
+            for (let i = 0; i < DEFAILT_SETTINGS[nameGame].countRound; i++) {
+                let temp = await this[`start${nameGame}ShowScreen`]();
+                let temp2 = await this[`start${nameGame}GuessScreen`]();
+                this.score += temp == temp2 ? 1 : 0;
+                this.count += 1;
+            };
+            this.screen = "gameEnd";
+        },
+        async startMultiGame() {
+            for (let i = 0; i < DEFAILT_SETTINGS.MultiGame.countRound; i++) {
+                let game = getRandomVal(DEFAILT_SETTINGS.MultiGame.games);
+                let temp = await this[`start${game}ShowScreen`]();
+                let temp2 = await this[`start${game}GuessScreen`]();
+                this.count += 1;
+                this.score += temp == temp2 ? 1 : 0;
+            };
+        },
     },
-    created() {
-        window.addEventListener("hashchange", () => {
-            this.hash = location.hash;
-        });
+    async created() {},
+    async mounted() {
+        this.$refs.form.addEventListener("keydown", event =>
+            event.keyCode == 13 ? this.$refs.btn.click() : 0
+        );
     },
 }).mount("#app");
