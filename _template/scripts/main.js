1ae09bb88d5cd924f607326bfae2dfcc72a20bb1
Denis <qwertyuiop852002058@yandex.ru>
2021-06-13 01:59:52 +0300

    3

--- _template/scripts/main.js
+++ _template/scripts/main.js
@@ -1,18 +1,34 @@
 DEFAILT_SETTINGS = {
     "CarNumber": {
-        timeShow: 7,
-        countRound: 5,
-    },
-    "MultiGame": {
-        countRound: 10,
-        games: ["CarNumber"],
-    },
-    games: [{
         name: "CarNumber",
         title: "Автономильный номер",
         description: "Запомните, а потом вспомните, автоноьильный номер в формате России",
-    }],
+        timeShow: 7,
+        countRound: 5,
+    },
+    "PhoneNumber": {
+        name: "PhoneNumber",
+        title: "Телефоный номер",
+        description: "Запомните, а потом вспомните, телефоный номер в международном формате (РФ)",
+        timeShow: 7,
+        countRound: 5,
+    },
+    "Paragraph": {
+        name: "Paragraph",
+        title: "Параграф",
+        description: "Запомните, а потом вспомните, телефоный номер в международном формате (РФ)",
+        timeShow: 7,
+        countRound: 5,
+        max: 200,
+        min: 100,
+    },
+    $: {
+        countRound: 10,
+        games: ["CarNumber", "PhoneNumber", "Paragraph"],
+    },
 };
+
+const CACHE = {};
 
 function getRandomInt(max, min) {
     return Math.floor(Math.random() * (max - min - 1)) + min + 1;
@@ -29,11 +45,21 @@ function replaceSimilarLetters(str) {
     return str.replaceAll(new RegExp(`[${R}]`, "g"), o => E[R.indexOf(o)]);
 };
 
+async function getRandomParagraphFromBook(lang, min, max) {
+    if (!CACHE.books) {
+        CACHE.books = await fetch("{{ start_url }}/data/books.json");
+        CACHE.books = (await CACHE.books.json()).books;
+    };
+    const b = getRandomVal(CACHE.books.filter(i=> !lang || i.lang == lang));
+    let p = b.text.split("\n").filter(i => i.length > min && i.length < max);
+    p = getRandomVal(p);
+    return {name: b.name, text: p};
+};
+
 const vueApp = Vue.createApp({
     data: () => ({
         settings: DEFAILT_SETTINGS,
         screen: "none",
-        options: {},
         time: 5,
         score: 0,
         count: 0,
@@ -43,7 +69,7 @@ const vueApp = Vue.createApp({
             const L = "ABEKMHOPCTYX";
             let number = getRandomVal(L) + getRandomInt(100, 999);
             number += getRandomVal(L) + getRandomVal(L) + " " + getRandomInt(1, 999);
-            this.options.number = number;
+            this.$refs["car-number-number"].textContent = number;
             this.time = DEFAILT_SETTINGS.CarNumber.timeShow;
             this.screen = "carNumberShow";
             while (this.time) {
@@ -51,20 +77,63 @@ const vueApp = Vue.createApp({
                 this.time -= 1;
             };
             this.screen = "none";
-            this.options = {};
             number = number.replaceAll(/\s*/g, "").toUpperCase();
             number = replaceSimilarLetters(number);
             return number;
         },
         async startCarNumberGuessScreen() {
             this.screen = "carNumberGuess";
-            setTimeout(() => this.$refs.input.focus(), 500);
-            await new Promise(resolve => this.$refs.btn.onclick = resolve);
-            let answer = this.options.answer || "";
+            setTimeout(() => this.$refs["car-number-input"].focus(), 500);
+            await new Promise(r => this.$refs["car-number-btn"].onclick = r);
+            let answer = this.$refs["car-number-input"].value || "";
+            this.$refs["car-number-input"].value = "";
             answer = answer.replaceAll(/\s*/g, "").toUpperCase();
             answer = replaceSimilarLetters(answer);
             this.screen = "none";
-            this.options = {};
+            return answer;
+        },
+        async startPhoneNumberShowScreen() {
+            const number = "+7" + getRandomInt(1_000_000_000, 9_999_999_999);
+            this.$refs["phone-number-number"].textContent = number;
+            this.time = DEFAILT_SETTINGS.PhoneNumber.timeShow;
+            this.screen = "phoneNumberShow";
+            while (this.time) {
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                this.time -= 1;
+            };
+            this.screen = "none";
+            return number.replaceAll("+7", "");
+        },
+        async startPhoneNumberGuessScreen() {
+            this.screen = "phoneNumberGuess";
+            setTimeout(() => this.$refs["phone-number-input"].focus(), 500);
+            await new Promise(r => this.$refs["phone-number-btn"].onclick = r);
+            const answer = this.$refs["phone-number-input"].value || "";
+            this.$refs["phone-number-input"].value = "";
+            this.screen = "none";
+            return answer.replaceAll("+7", "");
+        },
+        async startParagraphShowScreen() {
+            const s = this.settings.Paragraph;
+            const temp = await getRandomParagraphFromBook(null, s.min, s.max)
+            this.$refs["paragraph-text"].textContent = temp.text;
+            this.$refs["paragraph-name"].textContent = temp.name;
+            this.time = s.timeShow;
+            this.screen = "paragraphShow";
+            while (this.time) {
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                this.time -= 1;
+            };
+            this.screen = "none";
+            return temp.text;
+        },
+        async startParagraphGuessScreen() {
+            this.screen = "paragraphGuess";
+            setTimeout(() => this.$refs["paragraph-input"].focus(), 500);
+            await new Promise(r => this.$refs["paragraph-btn"].onclick = r);
+            const answer = this.$refs["paragraph-input"].value || "";
+            this.$refs["paragraph-input"].value = "";
+            this.screen = "none";
             return answer;
         },
         async startPlainGame(nameGame) {
@@ -77,19 +146,28 @@ const vueApp = Vue.createApp({
             this.screen = "gameEnd";
         },
         async startMultiGame() {
-            for (let i = 0; i < DEFAILT_SETTINGS.MultiGame.countRound; i++) {
-                let game = getRandomVal(DEFAILT_SETTINGS.MultiGame.games);
+            for (let i = 0; i < DEFAILT_SETTINGS.$.countRound; i++) {
+                let game = getRandomVal(DEFAILT_SETTINGS.$.games);
                 let temp = await this[`start${game}ShowScreen`]();
                 let temp2 = await this[`start${game}GuessScreen`]();
                 this.count += 1;
                 this.score += temp == temp2 ? 1 : 0;
             };
+            this.screen = "gameEnd";
         },
     },
-    async created() {},
-    async mounted() {
-        this.$refs.form.addEventListener("keydown", event =>
-            event.keyCode == 13 ? this.$refs.btn.click() : 0
-        );
+    computed: {
+        games(vm) {
+            const games = Object.assign({}, vm.settings);
+            delete games["$"];
+            return games;
+        },
+    },
+    mounted() {
+        document.querySelectorAll(".form").forEach(el => {
+            el.addEventListener("keydown", event =>
+                event.keyCode == 13 ? el.querySelector(".btn").click() : 0
+            );
+        });
     },
 }).mount("#app");
